425afc13866ecb5994725ab5d8e7e07bd4de5251
WW <zhanaiger2001@gmail.com>
2020-10-09 22:41:11 +0600

    i did it !

--- sources/main.c
+++ sources/main.c
@@ -31,11 +31,11 @@ char *get_word(char *end) {
 }
 
 char **get_list() {
-    char **list = NULL;
     char end;
+    char **list = NULL;
     char *word = get_word(&end);
     int i = 0;
-    while (word) {
+    while (1) {
         if (end == '\n') {
             if (strlen(word) == 0) {
                 list  = realloc(list, (i + 1) * sizeof(char*));
@@ -53,20 +53,23 @@ char **get_list() {
         list[i - 1] = word;
         word = get_word(&end);
     }
-//    arr = realloc(arr, (i + 1) * sizeof(char*));
-  //  arr[i - 1] = NULL;
+  //  list = realloc(list, i * sizeof(char*));
+//    list[i - 1] = NULL;
     return list;
 }
 
 void redirection(char **list) {
     int i = 0, fd_in = 0, fd_out = 1;
-    while (1) {
+    while (list[i]) {
         if (strcmp(list[i], "<") == 0) {
             fd_in = open(list[i + 1], O_RDONLY);
             if (fd_in < 0) {
                 perror("Open failed");
                 exit(1);
             }
+            dup2(fd_in, 0);
+            free(list[i]);
+            list[i] = NULL;
             break;
         } else if (strcmp(list[i], ">") == 0) {
             fd_out = open(list[i + 1], O_WRONLY | O_CREAT | O_TRUNC,
@@ -75,12 +78,15 @@ void redirection(char **list) {
                 perror("Open failed");
                 exit(1);
             }
+            dup2(fd_out, 1);
+            free(list[i]);
+            list[i] = NULL;
             break;
         }
         i++;
     }
-    list = realloc(list, i * sizeof(char*));
-    list[i] = NULL;
+   // list = realloc(list, i * sizeof(char*));
+   // list[i] = NULL;
     return;
 }
 
@@ -92,22 +98,39 @@ void clear(char **list) {
     }
     free(list);
 }
+/*
+int main() {
+	char **list;
+	while(1) {
+		list = get_list();
+		if ((strcmp(list[0], "exit") == 0) || (strcmp(list[0], "quit") == 0)) {
+			clear(list);
+			break;
+		}
+		redirection(list);
+		clear(list);
+	}
+	return 0;
+}
+*/
 
 int main() {
-     int fd_in = 0, fd_out = 1;
+//     int fd_in = 0, fd_out = 1;
      char **list = get_list();
-     while((strcmp(list[0], "exit") == 0) || (strcmp(list[0], "quit") == 0)) {
-         if (fork() > 0) {
-             wait(NULL);
-         } else {
-             dup2(fd_in, 0);
-             dup2(fd_out, 1);
-             if (execvp(list[0], list) < 0) {
-                 perror("exec failed");
-                return 1;
-             }
-         }
-         
+     while ((strcmp(list[0], "exit") && strcmp(list[0], "quit")) || (!list[0])) {
+        if (list[0]) {
+            if (fork() > 0) {
+                wait(NULL);
+            } else {
+                redirection(list);
+                if (execvp(list[0], list) < 0) {
+                    perror("exec failed");
+                    return 1;
+                }
+            } 
+        }
+        clear(list);
+        list = get_list();
      }
      clear(list);
      return 0;
